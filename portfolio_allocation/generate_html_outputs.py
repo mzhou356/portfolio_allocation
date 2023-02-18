8f927a1dbec509be0bc21c4d43cb3ebd721bf772
mindy <minzhou16@gmail.com>
2023-02-17 22:27:43 -0500

    Added function signature.
    Signed-off-by: mindy <minzhou16@gmail.com>

--- portfolio_allocation/generate_html_outputs.py
+++ portfolio_allocation/generate_html_outputs.py
@@ -3,6 +3,9 @@ browser with allocation information."""
 
 import webbrowser
 from pathlib import Path
+from typing import List
+
+import pandas as pd
 
 ROOT_DIR: Path = Path(__file__).parent
 
@@ -17,3 +20,28 @@ def open_local_html(local_path: str = DEFAULT_PATH) -> None:
         DEFAULT PATH.
     """
     webbrowser.open(url="file://" + str(ROOT_DIR / local_path))
+
+
+def _convert_pdf_to_html_table(asset_table: pd.DataFrame) -> List[str]:
+    """
+    Returns a list of html text as a table.
+    Args:
+        asset_table (pd.DataFrame): asset allocation table.
+    Returns:
+        a list of html text for the html table.
+    """
+    return ["a", "b"]
+
+
+def update_asset_allocation_html(
+    asset_table_without_region: pd.DataFrame,
+    asset_table_with_region: pd.DataFrame,
+    file_path: str,
+) -> None:
+    """
+    This function updates the existing html table.
+    Args:
+        asset_table_without_region (pd.DataFrame): asset allocation table without region information.
+        asset_table_with_region (pd.DataFrame): asset allocation table with region information.
+        file_path (str): local file path for the html file.
+    """
