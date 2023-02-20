e30d3c1a32bb496737636f0eba465137edf3151d
mindy <minzhou16@gmail.com>
2023-02-20 14:49:24 -0500

    Added fixtures and added function for process_blend_fund_texts.
    Signed-off-by: mindy <minzhou16@gmail.com>

--- tests/blend_fund_asset_allocation_test.py
+++ tests/blend_fund_asset_allocation_test.py
@@ -5,6 +5,8 @@ from portfolio_allocation.blend_fund_asset_allocation import (
     _relevant_page_filter,
     _process_fund_name_columns,
     _extract_dollar_amount,
+    _extract_fund_value_from_text_line,
+    _process_blend_fund_texts,
 )
 
 
@@ -17,7 +19,7 @@ from portfolio_allocation.blend_fund_asset_allocation import (
         (6, {4, 5, 6}, True),
     ],
 )
-def test_relevant_page_filter_succeeds(page_num, target_page_num, expected):
+def test_relevant_page_filter_succeeds(page_num, target_page_num, expected) -> None:
     """Test relevant_page_filter function."""
     actual = _relevant_page_filter(
         page_number=page_num,
@@ -36,7 +38,7 @@ def test_relevant_page_filter_succeeds(page_num, target_page_num, expected):
 )
 def test_process_fund_name_columns_succeeds(
     text_line, start_index, end_index, expected
-):
+) -> None:
     """Test _process_fund_name_columns function."""
     actual = _process_fund_name_columns(
         text_with_fund_name=text_line,
@@ -57,10 +59,58 @@ def test_process_fund_name_columns_succeeds(
         (".25", "", 0.25),
     ],
 )
-def test_extract_dollar_amount_succeeds(asset_amount, chars_to_strip, expected):
+def test_extract_dollar_amount_succeeds(asset_amount, chars_to_strip, expected) -> None:
     """Test extract_dollar_amount."""
     actual = _extract_dollar_amount(
         asset_amount=asset_amount,
         chars_to_strip=chars_to_strip,
     )
     assert expected == actual
+
+
+@pytest.mark.parametrize(
+    "text_line, index_number , str_filter, expected",
+    [
+        ("fund A $100,000.25", -1, None, 100000.25),
+        ("info fund_B info $25,000.00 total 100.02", 0, "$", 25000.0),
+        ("fund B 100.0 fund A 240.25", 2, None, 100.0),
+    ],
+)
+def test_extract_fund_value_from_text_line(
+    text_line, index_number, str_filter, expected
+) -> None:
+    """Test function extract_fund_value_from_text_line"""
+    actual = _extract_fund_value_from_text_line(
+        text_line=text_line,
+        fund_value_index_number=index_number,
+        amount_str_filter=str_filter,
+    )
+    assert expected == actual
+
+
+def test_process_blend_fund_texts(
+    pdf_statement_pages,
+    blend_fund_asset_allocation,
+    expected_text_fund_asset_allocation,
+    mocker,
+) -> None:
+    """Test _process_blend_fund_texts"""
+    mocker.patch(
+        "portfolio_allocation.blend_fund_asset_allocation.load_pdf_statements",
+        return_value=pdf_statement_pages,
+        name="mock_load_pdf_statement_method",
+    )
+    fund_names = ["A", "B", "C", "E", "F"]
+    target_page_nums = {1, 2}
+    file_path = "test_file_path"
+    fund_value_index_number = -1
+
+    actual = _process_blend_fund_texts(
+        file_path=file_path,
+        target_page_num=target_page_nums,
+        blend_fund_asset_allocation=blend_fund_asset_allocation,
+        fund_names=fund_names,
+        fund_value_index_number=fund_value_index_number,
+    )
+
+    assert expected_text_fund_asset_allocation == actual
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,56 @@
+# pylint: disable = missing-function-docstring, redefined-outer-name,
+# pylint: disable = missing-module-docstring
+from typing import List, Dict
+
 import pytest
+
+
+@pytest.fixture(scope="function")
+def pdf_text_page_one() -> str:
+    return """
+    fund_A info_1 info_2 $250.27
+    fund_B info_1 info_2 $36.28
+    fund_C info_1 info_2 $.25
+    """
+
+
+@pytest.fixture(scope="function")
+def pdf_text_page_two() -> str:
+    return """
+    fund_E info_3 info_4 $1,250.27
+    fund_F info_3 info_4 $125,036.28
+    """
+
+
+@pytest.fixture(scope="function")
+def pdf_statement_pages(pdf_text_page_one, pdf_text_page_two, mocker) -> List:
+    page_zero = mocker.Mock()
+    page_one = mocker.Mock()
+    page_two = mocker.Mock()
+    page_zero.extract_text.return_value = ""
+    page_one.extract_text.return_value = pdf_text_page_one
+    page_two.extract_text.return_value = pdf_text_page_two
+    return [page_zero, page_one, page_two]
+
+
+@pytest.fixture(scope="function")
+def blend_fund_asset_allocation() -> Dict[str, Dict[str, float]]:
+    return {
+        "A": {"us_stock": 1},
+        "B": {"international_stock": 0.75, "fixed_income": 0.25},
+        "C": {"other": 0.25, "us_stock": 0.50, "fixed_income": 0.25},
+        "E": {"not_classified": 0.5, "fixed_income": 0.5},
+        "F": {"us_stock": 0.75, "international_stock": 0.25},
+    }
+
+
+@pytest.fixture(scope="function")
+def expected_text_fund_asset_allocation() -> Dict[str, float]:
+    return {
+        "us_stock": 94027.605,
+        "international_stock": 31286.28,
+        "fixed_income": 634.2675,
+        "other": 0.0625,
+        "not_classified": 625.135,
+        "cash": 0.0,
+    }
