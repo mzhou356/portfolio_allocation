c80486ff00adcfd9cef4d322ea243c909e3c15b7
mindy <minzhou16@gmail.com>
2023-02-20 13:41:54 -0500

    Added test for _process_fund_name_columns function.
    Signed-off-by: mindy <minzhou16@gmail.com>

--- tests/blend_fund_asset_allocation_test.py
+++ tests/blend_fund_asset_allocation_test.py
@@ -1,7 +1,10 @@
 """This module tests all functions from module blend_fund_asset_allocation."""
 import pytest
 
-from portfolio_allocation.blend_fund_asset_allocation import _relevant_page_filter
+from portfolio_allocation.blend_fund_asset_allocation import (
+    _relevant_page_filter,
+    _process_fund_name_columns,
+)
 
 
 @pytest.mark.parametrize(
@@ -18,6 +21,24 @@ def test_relevant_page_filter_succeeds(page_num, target_page_num, expected):
     actual = _relevant_page_filter(
         page_number=page_num,
         target_page_num=target_page_num,
+    )
+
+    assert expected == actual
+
+
+@pytest.mark.parametrize(
+    "text_line, start_index , end_index, expected",
+    [
+        ("fund A $100,00", 0, 2, "fund A"),
+        ("info fund_B info $25,000.00 total", 1, 2, "fund_B"),
+    ],
+)
+def test_process_fund_name_columns(text_line, start_index, end_index, expected):
+    """Test _process_fund_name_columns function."""
+    actual = _process_fund_name_columns(
+        text_with_fund_name=text_line,
+        fund_col_index_start=start_index,
+        fund_col_index_end=end_index,
     )
 
     assert expected == actual
