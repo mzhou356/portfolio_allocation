d788eb8d1efe7a5c5ff5a40893be01824d70cdf0
mindy <minzhou16@gmail.com>
2023-02-22 21:30:33 -0500

    Added test for generate_combined_non_blend_fund_asset_allocation.
    Signed-off-by: mindy <minzhou16@gmail.com>

--- tests/non_blend_fund_asset_allocation_test.py
+++ tests/non_blend_fund_asset_allocation_test.py
@@ -7,6 +7,7 @@ import pytest
 from portfolio_allocation.non_blend_fund_asset_allocation import (
     _estimate_mortgage_asset,
     _create_non_blend_fund_asset_allocation,
+    generate_combined_non_blend_fund_asset_allocation,
 )
 
 
@@ -33,5 +34,29 @@ def test_create_non_blend_fund_asset_allocation(account_name, expected, mocker) 
     input_mock_method.side_effect = ["10000", "8000"]
 
     actual = _create_non_blend_fund_asset_allocation(account_name=account_name)
+
+    assert actual == expected
+
+
+def test_generate_combined_non_blend_fund_asset_allocation(mocker) -> None:
+    """Test generate_combined_non_blend_fund_asset_allocation."""
+    input_mock_method = mocker.patch.object(builtins, "input")
+    input_mock_method.side_effect = ["1000", "1000", "1000", "1000", "1000"]
+    mocker.patch(
+        "portfolio_allocation.non_blend_fund_asset_allocation."
+        "_estimate_mortgage_asset",
+        return_value=2000.0,
+    )
+    expected = {
+        "us_stock": 1000.0,
+        "cash": 3000.0,
+        "mortgage": 2000.0,
+        "fixed_income": 1000.0,
+        "international_stock": 0,
+        "other": 0,
+        "not_classified": 0,
+    }
+
+    actual = generate_combined_non_blend_fund_asset_allocation()
 
     assert actual == expected
