a46f36b41976bd00af564b7e3e2c7c3cf8caff0d
mindy <minzhou16@gmail.com>
2023-02-22 21:22:40 -0500

    Added test for _create_non_blend_fund_asse_allocation.
    Signed-off-by: mindy <minzhou16@gmail.com>

--- tests/non_blend_fund_asset_allocation_test.py
+++ tests/non_blend_fund_asset_allocation_test.py
@@ -2,14 +2,15 @@
 """This module tests all functions from module non_blend_fund_asset_allocation."""
 
 import builtins
-
+import pytest
 
 from portfolio_allocation.non_blend_fund_asset_allocation import (
     _estimate_mortgage_asset,
+    _create_non_blend_fund_asset_allocation,
 )
 
 
-def test_estimate_mortgage_asset(mocker):
+def test_estimate_mortgage_asset(mocker) -> None:
     """Test function estimate_mortgage_asset."""
     input_mock_method = mocker.patch.object(builtins, "input")
     input_mock_method.side_effect = ["60000", "58000"]
@@ -17,3 +18,20 @@ def test_estimate_mortgage_asset(mocker):
     actual = _estimate_mortgage_asset()
 
     assert actual == 2000
+
+
+@pytest.mark.parametrize(
+    "account_name, expected",
+    [
+        ("t_rowe_price_group_stock", {"us_stock": 10000.0}),
+        ("mortgage", {"mortgage": 2000}),
+    ],
+)
+def test_create_non_blend_fund_asset_allocation(account_name, expected, mocker) -> None:
+    """Test function create_non_blend_fund_asset_allocation."""
+    input_mock_method = mocker.patch.object(builtins, "input")
+    input_mock_method.side_effect = ["10000", "8000"]
+
+    actual = _create_non_blend_fund_asset_allocation(account_name=account_name)
+
+    assert actual == expected
